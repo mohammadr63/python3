7bc760b474ce80a52a521244dbb8cec9b40d26fc
mammad121 <mohammadraoufi84@gmail.com>
2021-07-10 00:20:50 +0430

    Time on python

---
+++ Time.py
@@ -0,0 +1,12 @@
+import datetime
+
+now = datetime.datetime.today()
+
+mm = str(now.month)
+dd = str(now.day)
+yyyy = str(now.year)
+hour = str(now.hour)
+mi = str(now.minute)
+ss = str(now.second)
+
+print (mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
